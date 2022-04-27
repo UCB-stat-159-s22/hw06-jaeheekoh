ce03ebfd80bf4921ef5d8e8d3b0e65e62fdca3a1
Jae Hee Koh <jaeheekoh@berkeley.edu>
2022-04-27 04:51:41 +0000

    everything until jupyter book

---
+++ ligotools/tests/test_readligo.py
@@ -0,0 +1,13 @@
+from ligotools import readligo as rl
+import os
+
+"""
+def test_toy_default():
+    assert toy() == 1
+
+def test_toy_0():
+    assert toy(0) == 1
+
+def test_toy_1():
+    assert toy(1) == 2
+    """
