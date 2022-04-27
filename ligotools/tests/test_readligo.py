1d2f4fa01270e35ff90cbec69a6b087bda7c91c8
Jae Hee Koh <jaeheekoh@berkeley.edu>
2022-04-27 21:00:35 +0000

    done?

--- ligotools/tests/test_readligo.py
+++ ligotools/tests/test_readligo.py
@@ -1,13 +1,15 @@
 from ligotools import readligo as rl
+import numpy as np
 import os
 
-"""
-def test_toy_default():
-    assert toy() == 1
-
-def test_toy_0():
-    assert toy(0) == 1
-
-def test_toy_1():
-    assert toy(1) == 2
-    """
+def test_readhdf5_L1():
+    l1 = rl.read_hdf5('data/L-L1_LOSC_4_V2-1126259446-32.hdf5')
+    assert len(l1) == 7
+    
+def test_readhdf5_H1():
+    h1 = rl.read_hdf5('data/H-H1_LOSC_4_V2-1126259446-32.hdf5')
+    assert len(h1) == 7
+    
+def test_getsegs():
+    strain_H1, time_H1, chan_dict_H1 = rl.loaddata(filepath, 'H1')
+    assert(len(strain_H1) == 131072)
